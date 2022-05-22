7ca9ed9861ce10fd75ad20e1449e9f358933bf6c
João Távora <joaotavora@gmail.com>
2022-05-22 12:38:14 +0100

    x3-4

---
+++ src/x3-4.cpp
@@ -0,0 +1,13 @@
+/**
+   Q: List three (or more) C++ compilers.
+
+   A: g++, from the GCC Gnu Compiler Collection
+      clang++, from the Clang C Language Family Frontend for LLVM
+      MSVC Microsoft Visual C++ (usually invoked through MS Visual Studio)
+*/
+
+#ifdef EXERCISE_MAIN
+int main() {
+  return 0;
+}
+#endif
