280d459770240dd135a866f76530e7165c6d1b67
João Távora <joaotavora@gmail.com>
2022-05-22 13:32:38 +0100

    x4-2

---
+++ src/x4-2.cpp
@@ -0,0 +1,25 @@
+/**
+   Q: Give five examples of concrete types that are built-in types in
+      C++. Give five examples of concrete types that are not built-in
+      types in C++.
+
+   A:
+
+   'char', stores (or helps store) code of a member of a given character set.
+   'int', a type for representing integer numbers
+   'long', another such type
+   'bool', for representing a boolean value
+   'float', for numbers with fractional parts within a given precision.
+
+   a 'Matrix' type
+   an 'EmployeeRecord' type
+   a 'TemperatureMeasurement' type
+   an 'OrcWarrior' type
+   a 'FileHandle' type
+*/
+
+#ifdef EXERCISE_MAIN
+int main() {
+  return 0;
+}
+#endif
